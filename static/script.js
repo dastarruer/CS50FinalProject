45184b04bb8156e8c0368853c0b5d4779e313466
Ayush Pramanik <ayushpramanik399@gmail.com>
2024-10-16 17:09:29 -0400

    Added functionality so that the app can now recognize a correctly/incorrectly typed word using js

--- static/script.js
+++ static/script.js
@@ -3,15 +3,23 @@ const input = document.getElementById('inputfield')
 // Check if the user presses space, at which point we assume they have typed a word
 input.addEventListener('keydown', (event) => {
     if (event.key == ' ') {
+        verifyWordTyped(input.value);
         // Clear the input field
         if (input.value != "") {
-            input.value = null;
+            input.value = "";
         }
-        verifyWordTyped();
     }
 })
 
-function verifyWordTyped()
+function verifyWordTyped(typedWord)
 {
-    console.log("Word typed");
+    let actualWord = document.getElementById('untyped').innerHTML
+
+    if (typedWord === actualWord)
+    {
+        console.log("correct!")
+    } 
+    else {
+        console.log("incorrect...")
+    }
 }
