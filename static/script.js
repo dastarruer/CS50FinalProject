2bc1892ffb13c439b90024d47d6db150324ca40a
Ayush Pramanik <ayushpramanik399@gmail.com>
2024-10-16 09:24:27 -0400

    The js works now, because the script tag is now placed after the html tag. before it was forcing the script to trigger before the html was loaded

--- static/script.js
+++ static/script.js
@@ -1,11 +1,11 @@
-const input = document.querySelector('input')
+const input = document.getElementById('inputfield')
 
 // Check if the user presses space, at which point we assume they have typed a word
 input.addEventListener('keydown', (event) => {
     if (event.key == ' ') {
         // Clear the input field
         if (input.value != "") {
-            input.value = "";
+            input.value = null;
         }
         verifyWordTyped();
     }
