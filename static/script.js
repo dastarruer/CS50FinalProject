8da4ae08517ed19205a3cb4eddc8e0216a439ace
Ayush Pramanik <ayushpramanik399@gmail.com>
2024-10-17 19:39:42 -0400

    Added a timer which times how long the user takes to type. In its current form, it can't stop, but ill get to that later. also changed untyped/typed-correct/typed-incorrect id's to classes so we aren't reusing id's

--- static/script.js
+++ static/script.js
@@ -1,27 +1,48 @@
-const input = document.getElementById('inputfield')
+const input = document.getElementById("inputfield");
 
-// Check if the user presses space, at which point we assume they have typed a word
-input.addEventListener('keydown', (event) => {
-    if (event.key == ' ') {
-        verifyWordTyped(input.value);
+let startTime = null;
+let timerInterval = null;
+let elapsedTime = null;
+let isTyping = false;
+
+input.addEventListener("keydown", (event) => {
+    // Check if the user presses space, at which point we assume they have typed a word
+    if (event.key == " ") {
+        verifyWordTyped(input.value)
         // Clear the input field
         if (input.value != "") {
-            input.value = "";
+            input.value = ""
         }
     }
 })
 
-function verifyWordTyped(typedWord)
-{
-    let actualWordTag = document.getElementById('untyped')
-    let actualWord = actualWordTag.innerHTML
 
-    if (typedWord.trim() === actualWord)
-    {
-        actualWordTag.id = "typed-correct"
-        console
-    } 
+input.addEventListener("input", () => {
+    // If the user has not started typing
+    if (!isTyping) {
+        // Get the time when the user started typing
+        startTime = new Date().getTime();
+
+        // Start a timer 
+        timerInterval = setInterval(() => {
+            // Get the elapsed time in milliseconds
+            elapsedTime = (new Date().getTime() - startTime) / 1000;
+        }, 1000);
+        isTyping = true;
+    } else {
+        console.log(elapsedTime)
+    }
+});
+
+
+function verifyWordTyped(typedWord) {
+    const actualWordElement = document.getElementsByClassName("untyped")[0]
+    const actualWord = actualWordElement.innerHTML
+
+    if (typedWord.trim() === actualWord) {
+        actualWordElement.className = "typed-correct"
+    }
     else {
-        actualWordTag.id = "typed-incorrect"
+        actualWordElement.className = "typed-incorrect"
     }
 }
