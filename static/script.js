97142e491566c7bb501d3fc57cedf7e4931cb52e
Ayush Pramanik <ayushpramanik399@gmail.com>
2024-10-17 20:19:08 -0400

    Actually added way to count typos; much more expanded now as it can count typos on strings with different lengths as well, also added autofocus to input tag in index.html

--- static/script.js
+++ static/script.js
@@ -11,14 +11,13 @@ input.addEventListener("keydown", (event) => {
     // Check if the user presses space, at which point we assume they have typed a word
     if (event.key == " ") {
         const actualWordElement = document.getElementsByClassName("untyped")[0];
-        const actualWord = actualWordElement.innerHTML;
 
         // If actualWordElement is null, then there are no more words to type
         if (actualWordElement === null) {
             clearInterval(timerInterval);
         }
 
-        verifyWordTyped(input.value, actualWord);
+        verifyWordTyped(input.value, actualWordElement);
         // Clear the input field
         if (input.value != "") {
             input.value = "";
@@ -38,13 +37,36 @@ input.addEventListener("input", () => {
             elapsedTime = (new Date().getTime() - startTime) / 1000;
         }, 1000);
         isTyping = true;
-    } else {
-        console.log(elapsedTime);
     }
 });
 
-function verifyWordTyped(typedWord, actualWord) {
-    let numOfTypos = Math.abs(typedWord.trim().localCompare(actualWord));
+// Get the number of typos between the typed word and the actual word
+function getTypos(typedWord, actualWord) {
+    
+    let numOfTypos = 0;
+
+    len = typedWord.length;
+    for (let i = 0; i < len; i++) {
+        typedChar = typedWord[i];
+        actualChar = actualWord[i];
+        
+        if (typedChar !== actualChar) {
+            numOfTypos++;
+        }
+        else if (actualChar === null) {
+            // If actualChar is null, that means that the typedWord is longer than actualWord
+            numOfTypos += typedWord.length - actualWord.length;
+            break;
+        }
+    }
+    return numOfTypos;
+}
+
+function verifyWordTyped(typedWord, actualWordElement) {
+    const actualWord = actualWordElement.innerHTML
+
+    typedWord = typedWord.trim()
+    let numOfTypos = getTypos(typedWord, actualWord);
 
     if (numOfTypos === 0) {
         actualWordElement.className = "typed-correct";
@@ -52,4 +74,5 @@ function verifyWordTyped(typedWord, actualWord) {
         actualWordElement.className = "typed-incorrect";
     }
     totalTypos += numOfTypos;
+    console.log(totalTypos);
 }
