b66aa999ab6eff310d3ba0a8f2fe576baa49b813
Ayush Pramanik <ayushpramanik399@gmail.com>
2024-10-20 13:50:43 -0400

    Added functionality to randomnly generate words from words.txt. THese words were taken from randomwordgenerator.com

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template\
+
+from random import sample
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    words = ["but", "because", "hello", "why", "there", "not", "perhaps"]
+    numOfWords = 30
+    words = []
+
+    # Open the list of words in words.txt
+    with open("words.txt", 'r') as file:
+        words = file.readlines()
+
+    words = sample(words, numOfWords)
+    words = [word.strip() for word in words]
+
     return render_template("index.html", words=words)
 
 if __name__ == '__main__':
